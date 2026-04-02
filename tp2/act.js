e5360b69ced70545389b5fd9d058b8d4a651611a
1samori <moriena699@gmail.com>
2026-04-02 12:07:18 -0300

    tp2

---
+++ tp2/act.js
@@ -0,0 +1,17 @@
+const nombre = prompt("Ingrese su nombre")
+let edad = parseInt(prompt("Ingrese su edad"))
+const contraseña = prompt("Ingrese su contraseña")
+
+if (nombre ===""){
+    console.log("Nombre invalido")
+}else if  (edad){ 
+    console.log("Acceso denegado")
+}else if  (contraseña.length < 6 ){
+    console.log("Contraseña inválida")
+}else {
+    console.log(`Bienvenido\nNombre = ${nombre}\nEdad = ${edad}\nContraseña = ${contraseña}`)
+    console.log(`Tu nombre es ${nombre} tienes ${edad} años y  tu contraseña es ${contraseña}`)
+    console.log(`En 10 años tendras ${edad + 10} años`)
+}
+
+
